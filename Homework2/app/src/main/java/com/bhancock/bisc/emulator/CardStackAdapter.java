a7fefe056e389933a98ff2a6e172ed045fc6f823
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-08-01 11:37:46 -0700

    Refactor/remove unnecessary code

--- Homework2/app/src/main/java/com/bhancock/bisc/emulator/CardStackAdapter.java
+++ Homework2/app/src/main/java/com/bhancock/bisc/emulator/CardStackAdapter.java
@@ -19,20 +19,11 @@ public class CardStackAdapter extends StackAdapter<Integer> {
 
     @Override
     public void bindView(Integer data, int position, CardStackView.ViewHolder holder) {
-        if (holder instanceof ColorItemLargeHeaderViewHolder) {
-            ColorItemLargeHeaderViewHolder h = (ColorItemLargeHeaderViewHolder) holder;
-            h.onBind(data, position);
-        }
-        if (holder instanceof ColorItemWithNoHeaderViewHolder) {
-            ColorItemWithNoHeaderViewHolder h = (ColorItemWithNoHeaderViewHolder) holder;
-            h.onBind(data, position);
-        }
         if (holder instanceof ColorItemViewHolder) {
             ColorItemViewHolder h = (ColorItemViewHolder) holder;
             h.onBind(data, position);
         }
     }
-
 
     @Override
     protected CardStackView.ViewHolder onCreateView(ViewGroup parent, int viewType) {
@@ -66,71 +57,8 @@ public class CardStackAdapter extends StackAdapter<Integer> {
         public void onBind(Integer data, int position) {
             mLayout.getBackground().setColorFilter(ContextCompat.getColor(getContext(), data), PorterDuff.Mode.SRC_IN);
             mTextTitle.setText(String.valueOf(position));
+
         }
 
     }
-
-    static class ColorItemWithNoHeaderViewHolder extends CardStackView.ViewHolder {
-        View mLayout;
-        TextView mTextTitle;
-
-        public ColorItemWithNoHeaderViewHolder(View view) {
-            super(view);
-            mLayout = view.findViewById(R.id.frame_list_card_item);
-            mTextTitle = (TextView) view.findViewById(R.id.text_list_card_title);
-        }
-
-        @Override
-        public void onItemExpand(boolean b) {
-        }
-
-        public void onBind(Integer data, int position) {
-            mLayout.getBackground().setColorFilter(ContextCompat.getColor(getContext(), data), PorterDuff.Mode.SRC_IN);
-            mTextTitle.setText(String.valueOf(position));
-        }
-
-    }
-
-    static class ColorItemLargeHeaderViewHolder extends CardStackView.ViewHolder {
-        View mLayout;
-        View mContainerContent;
-        TextView mTextTitle;
-
-        public ColorItemLargeHeaderViewHolder(View view) {
-            super(view);
-            mLayout = view.findViewById(R.id.frame_list_card_item);
-            mContainerContent = view.findViewById(R.id.container_list_content);
-            mTextTitle = (TextView) view.findViewById(R.id.text_list_card_title);
-        }
-
-        @Override
-        public void onItemExpand(boolean b) {
-            mContainerContent.setVisibility(b ? View.VISIBLE : View.GONE);
-        }
-
-        @Override
-        protected void onAnimationStateChange(int state, boolean willBeSelect) {
-            super.onAnimationStateChange(state, willBeSelect);
-            if (state == CardStackView.ANIMATION_STATE_START && willBeSelect) {
-                onItemExpand(true);
-            }
-            if (state == CardStackView.ANIMATION_STATE_END && !willBeSelect) {
-                onItemExpand(false);
-            }
-        }
-
-        public void onBind(Integer data, int position) {
-            mLayout.getBackground().setColorFilter(ContextCompat.getColor(getContext(), data), PorterDuff.Mode.SRC_IN);
-            mTextTitle.setText(String.valueOf(position));
-
-//            itemView.findViewById(R.id.text_view).setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    ((CardStackView)itemView.getParent()).performItemClick(ColorItemLargeHeaderViewHolder.this);
-//                }
-//            });
-        }
-
-    }
-
 }
