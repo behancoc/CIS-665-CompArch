ba20cedfbcc2f221c382363b97e61623d555738d
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-08-03 18:18:49 -0700

    RecyclerView list to display results when user runs execution

---
+++ Homework2/app/src/main/java/com/bhancock/bisc/emulator/adapters/ResultAdapter.java
@@ -0,0 +1,123 @@
+package com.bhancock.bisc.emulator.adapters;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bhancock.bisc.emulator.R;
+import com.bhancock.bisc.emulator.models.Instruction;
+
+public class ResultAdapter extends ListAdapter<Instruction, ResultAdapter.ResultsHolder> {
+
+    private final String TAG = ResultAdapter.class.getSimpleName();
+
+    private static final DiffUtil.ItemCallback<Instruction> DIFF_UTIL_INSTRUCTION_CALLBACK = new DiffUtil.ItemCallback<Instruction>() {
+        @Override
+        public boolean areItemsTheSame(@NonNull Instruction oldItem, @NonNull Instruction newItem) {
+            if (oldItem.getId() == newItem.getId()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Instruction oldItem, @NonNull Instruction newItem) {
+            if (oldItem.getInstructionFormat().equals(newItem.getInstructionFormat()) &&
+                oldItem.getOpcode().equals(newItem.getOpcode())) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    };
+
+    private OnItemClickListener onItemClickListener;
+
+    public ResultAdapter() {
+        super(DIFF_UTIL_INSTRUCTION_CALLBACK);
+    }
+
+    @NonNull
+    @Override
+    public ResultsHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.result_item, parent, false);
+
+        return new ResultsHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ResultsHolder holder, int position) {
+        Instruction currentInstruction = getItem(position);
+
+        holder.textViewOpcodeResult.setText(currentInstruction.getInstructionFormat());
+        holder.textViewDestRegResultValue.setText(currentInstruction.getDestinationRegister() + " 0x000");
+        holder.textViewSrc1RegResultValue.setText(currentInstruction.getSourceRegister1() + " 0x001");
+        holder.textViewSrc2RegResultValue.setText(currentInstruction.getSourceRegister2() + " 0x010");
+        holder.textViewOpcodeResultValue.setText(currentInstruction.getOpcode());
+
+    }
+
+    public Instruction getInstructionAt(int position) {
+        return getItem(position);
+    }
+
+    class ResultsHolder extends RecyclerView.ViewHolder {
+        private TextView textViewOpcodeResult;
+        private TextView textViewDestRegResult;
+        private TextView textViewSrc1RegResult;
+        private TextView textViewSrc2RegResult;
+        private TextView textViewExecutionResult;
+
+        private TextView textViewOpcodeResultValue;
+        private TextView textViewDestRegResultValue;
+        private TextView textViewSrc1RegResultValue;
+        private TextView textViewSrc2RegResultValue;
+        private TextView textViewExecutionResultValue;
+
+
+
+        public ResultsHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewOpcodeResult = itemView.findViewById(R.id.text_view_opcode_result);
+            textViewDestRegResult = itemView.findViewById(R.id.text_view_dest_register_result);
+            textViewSrc1RegResult = itemView.findViewById(R.id.text_view_source_register1_result);
+            textViewSrc2RegResult = itemView.findViewById(R.id.text_view_source_register2_result);
+            textViewExecutionResult = itemView.findViewById(R.id.text_view_source_instruction_result);
+
+            textViewOpcodeResultValue = itemView.findViewById(R.id.text_view_opcode_result_value);
+            textViewDestRegResultValue = itemView.findViewById(R.id.text_view_dest_register_result_value);
+            textViewSrc1RegResultValue = itemView.findViewById(R.id.text_view_source_register1_result_value);
+            textViewSrc2RegResultValue = itemView.findViewById(R.id.text_view_source_register2_result_value);
+            textViewExecutionResultValue = itemView.findViewById(R.id.text_view_source_instruction_result_value);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+
+                    try {
+                        onItemClickListener.onItemClick(getItem(position));
+                    } catch (NullPointerException e) {
+                        Log.e(TAG, "Null Pointer just occurred " + e.getMessage());
+                    }
+                }
+            });
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(Instruction instruction);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+}
--- Homework2/app/src/main/java/com/bhancock/bisc/emulator/views/ResultActivity.java
+++ Homework2/app/src/main/java/com/bhancock/bisc/emulator/views/ResultActivity.java
@@ -5,19 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.LoginFilter;
 import android.util.Log;
 
+import com.bhancock.bisc.emulator.adapters.InstructionAdapter;
+import com.bhancock.bisc.emulator.adapters.ResultAdapter;
 import com.bhancock.bisc.emulator.models.Instruction;
 import com.bhancock.bisc.emulator.viewmodels.InstructionViewModel;
 import com.bhancock.bisc.emulator.R;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -31,9 +38,23 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
-        instructionViewModel = ViewModelProviders.of(this).get(InstructionViewModel.class);
+        RecyclerView recyclerView = findViewById(R.id.results_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(false);
 
-        createMapping();
+        final ResultAdapter adapter = new ResultAdapter();
+        recyclerView.setAdapter(adapter);
+
+        instructionViewModel = ViewModelProviders.of(this).get(InstructionViewModel.class);
+        instructionViewModel.getAllInstructions().observe(this, new Observer<List<Instruction>>() {
+            @Override
+            public void onChanged(List<Instruction> instructions) {
+                adapter.submitList(instructions);
+            }
+        });
+
+
+
         readInstruction();
     }
 
@@ -51,10 +72,42 @@ public class ResultActivity extends AppCompatActivity {
             @Override
             public void onChanged(List<Instruction> instructions) {
                 if (instructions != null) {
-                    Log.d(TAG, "Happy Dance!");
 
-                    Log.d(TAG, "Grabbing some formats!: " +instructions.get(0).getInstructionFormat());
-                    //instructions.get(1).getDestinationRegister()
+                    for(int i = 0; i < instructions.size(); i ++) {
+
+                        Log.d(TAG, "Grabbing some formats!: " +instructions.get(i).getInstructionFormat());
+
+                        String key = instructions.get(i).getOpcode().toLowerCase();
+                        String formatCheck = instructions.get(i).getInstructionFormat().toLowerCase();
+
+                        if(instructionMapping.containsKey(key)) {
+                            continue;
+                        } else {
+
+                            if(formatCheck.equalsIgnoreCase("rrr-type")) {
+                                ArrayList<Object> assemblyFormat = new ArrayList<>();
+                                assemblyFormat.add(0, instructions.get(i).getDestinationRegister());
+                                assemblyFormat.add(1, instructions.get(i).getSourceRegister1());
+                                assemblyFormat.add(2, instructions.get(i).getSourceRegister2());
+
+                                instructionMapping.put(key, assemblyFormat);
+
+                            } else if (formatCheck.equalsIgnoreCase("rri-type")) {
+                                ArrayList<Object> assemblyFormat = new ArrayList<>();
+                                assemblyFormat.add(0, instructions.get(i).getDestinationRegister());
+                                assemblyFormat.add(1, instructions.get(i).getSourceRegister1());
+                                assemblyFormat.add(2, instructions.get(i).getSignedImmediate());
+                                instructionMapping.put(key, assemblyFormat);
+
+                            } else {
+                                ArrayList<Object> assemblyFormat = new ArrayList<>();
+                                assemblyFormat.add(0, instructions.get(i).getDestinationRegister());
+                                assemblyFormat.add(1, instructions.get(i).getImmediate());
+                            }
+                        }
+                    }
+
+
 
 
                 } else {
@@ -68,65 +121,9 @@ public class ResultActivity extends AppCompatActivity {
     private void computeInstructionResult(String opcode) {
     }
 
-    private void createMapping() {
+    private void createMapping(String opcode, String destinationReg, String sourceReg1,
+                               String sourceReg2, int signedImmed, int immed ) {
 
-
-
-        ArrayList<Object> addInstructionAssemblyFormat = new ArrayList<>();
-        addInstructionAssemblyFormat.add(0, "rA");
-        addInstructionAssemblyFormat.add(1, "rB");
-        addInstructionAssemblyFormat.add(2, "rC");
-
-
-        ArrayList<Object> addiInstructionAssemblyFormat = new ArrayList<>();
-        addiInstructionAssemblyFormat.add(0, "rA");
-        addiInstructionAssemblyFormat.add(1, "rB");
-        addiInstructionAssemblyFormat.add(2, "imm");
-
-
-        ArrayList<Object> nandInstructionAssemblyFormat = new ArrayList<>();
-        nandInstructionAssemblyFormat.add(0, "rA");
-        nandInstructionAssemblyFormat.add(1, "rB");
-        nandInstructionAssemblyFormat.add(2, "rC");
-
-        ArrayList<Object> luiInstructionAssemblyFormat = new ArrayList<>();
-        nandInstructionAssemblyFormat.add(0, "rA");
-        nandInstructionAssemblyFormat.add(1, "imm");
-
-        ArrayList<Object> swInstructionAssemblyFormat = new ArrayList<>();
-        nandInstructionAssemblyFormat.add(0, "rA");
-        nandInstructionAssemblyFormat.add(1, "rB");
-        nandInstructionAssemblyFormat.add(2, "imm");
-
-        ArrayList<Object> lwInstructionAssemblyFormat = new ArrayList<>();
-        nandInstructionAssemblyFormat.add(0, "rA");
-        nandInstructionAssemblyFormat.add(1, "rB");
-        nandInstructionAssemblyFormat.add(2, "imm");
-
-        ArrayList<Object> beqInstructionAssemblyFormat = new ArrayList<>();
-        nandInstructionAssemblyFormat.add(0, "rA");
-        nandInstructionAssemblyFormat.add(1, "rB");
-        nandInstructionAssemblyFormat.add(2, "imm");
-
-        ArrayList<Object> jalrInstructionAssemblyFormat = new ArrayList<>();
-        nandInstructionAssemblyFormat.add(0, "rA");
-        nandInstructionAssemblyFormat.add(1, "rB");
-
-
-
-        instructionMapping.put("add", addInstructionAssemblyFormat);
-        instructionMapping.put("addi", addiInstructionAssemblyFormat);
-        instructionMapping.put("nand", nandInstructionAssemblyFormat);
-        instructionMapping.put("lui", luiInstructionAssemblyFormat);
-        instructionMapping.put("sw", swInstructionAssemblyFormat);
-        instructionMapping.put("lw", lwInstructionAssemblyFormat);
-        instructionMapping.put("beq", beqInstructionAssemblyFormat);
-        instructionMapping.put("jalr", jalrInstructionAssemblyFormat);
-
-
-        Log.d(TAG, "Hey " + instructionMapping.get("addi"));
-
+        opcode = opcode.toLowerCase();
     }
-
-
 }
