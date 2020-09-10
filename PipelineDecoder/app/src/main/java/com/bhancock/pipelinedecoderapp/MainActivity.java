27905f7260a7185d4f88f4653ae0b07572c47b0c
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-09-10 15:49:36 -0700

    Draw UI on screen

--- PipelineDecoder/app/src/main/java/com/bhancock/pipelinedecoderapp/MainActivity.java
+++ PipelineDecoder/app/src/main/java/com/bhancock/pipelinedecoderapp/MainActivity.java
@@ -2,13 +2,38 @@ package com.bhancock.pipelinedecoderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Switch forwardingSwitch = (Switch) findViewById(R.id.forwarding_switch);
+        Button openFile = findViewById(R.id.open_file);
+
+        forwardingSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Handle Forwarding here...
+                Log.d(TAG, "Forwarding clicked");
+            }
+        });
+
+        openFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "Open File button Clicked");
+            }
+        });
     }
 }
