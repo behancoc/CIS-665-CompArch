1c17edc8e9e301630f3c17b535e6aa9edac4f003
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-09-10 16:36:14 -0700

    grabbing encoded uri path to document in downloads directory.

--- PipelineDecoder/app/src/main/java/com/bhancock/pipelinedecoderapp/MainActivity.java
+++ PipelineDecoder/app/src/main/java/com/bhancock/pipelinedecoderapp/MainActivity.java
@@ -1,18 +1,29 @@
 package com.bhancock.pipelinedecoderapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private static final int PICK_TEXT_FILE = 1;
+    private static final int OPEN_DOCUMENT_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +41,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         openFile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "Open File button Clicked");
                 Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
                 intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType("application/pdf");
-                startActivityForResult(intent, PICK_TEXT_FILE);
+                intent.setType("text/plain");
+                startActivityForResult(intent, OPEN_DOCUMENT_REQUEST_CODE);
             }
         });
+    }
+
+
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == OPEN_DOCUMENT_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Uri uri = null;
+            if (data != null) {
+                uri = data.getData();
+
+
+                Log.d(TAG, "Path: " +uri.getEncodedPath());
+            }
+        }
     }
 }
